56bc9a27de29da8642afbb01942bcd1fd7f787eb
Federico Farina <fedefarina02@gmail.com>
2021-10-12 19:30:07 -0300

    espejo retrovisor

---
+++ Assets/Scripts/ActivarRetrovisor.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivarRetrovisor : MonoBehaviour
+{
+    public GameObject texturaRetrovisor;
+    public GameObject retrovisor;
+
+    bool marchaAtras;
+
+    void Start()
+    {
+        retrovisor.SetActive(false);
+        texturaRetrovisor.SetActive(false);
+        marchaAtras = false;
+}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            if (marchaAtras == false)
+            {
+                retrovisor.SetActive(true);
+                texturaRetrovisor.SetActive(true);
+                marchaAtras = true;
+            }
+            else
+            {
+                retrovisor.SetActive(false);
+                texturaRetrovisor.SetActive(false);
+                marchaAtras = false;
+            }
+            
+        }
+    }
+}
---
+++ Assets/Scripts/EspejoRetrovisor.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EspejoRetrovisor : MonoBehaviour
+{
+    public Transform Auto;
+
+    float rotaciónX, rotaciónY, rotaciónZ;
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        rotaciónX = Auto.eulerAngles.x;
+        rotaciónY = Auto.eulerAngles.y;
+        rotaciónZ = Auto.eulerAngles.z;
+
+        transform.eulerAngles = new Vector3(rotaciónX - rotaciónX, rotaciónY, rotaciónZ - rotaciónZ);
+    }
+}
