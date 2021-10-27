111824c99a22419ef8f08b9768e50a439448f198
Federico Farina <fedefarina02@gmail.com>
2021-10-26 22:01:56 -0300

    Sonido Bocina

---
+++ Assets/Scripts/Bocina.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bocina : MonoBehaviour
+{
+
+    AudioSource audioSource;
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    
+    public void PulsarBocina()
+    {
+        audioSource.Play();
+    }
+
+    public void DetenerBocina()
+    {
+        audioSource.Stop();
+    }
+}
