80ce384b50d5eb51950bfa8feae0c550c7f9d995
Federico Farina <fedefarina02@gmail.com>
2021-11-02 18:05:31 -0300

    Spawn coches

---
+++ Assets/Scripts/SpawnearCoche.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class SpawnearCoche : MonoBehaviour
+{
+    public GameObject[] prefabCoches;
+    void Start()
+    {
+        Instantiate(ElegirPrefabAuto(), transform);
+    }
+
+    private GameObject ElegirPrefabAuto()
+    {
+        var indexRandom = Random.Range(0, prefabCoches.Length);
+        return prefabCoches[indexRandom];
+    }
+}
