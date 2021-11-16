6539059675d2b410fc85fdf4d44e6db2e8ed2ece
Federico Farina <fedefarina02@gmail.com>
2021-11-16 18:14:42 -0300

    Waypoints

---
+++ Assets/Scripts/Auto_Waypoint.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Auto_Waypoint : MonoBehaviour
+{
+    public float velocidad;
+
+    public Transform objetivo;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(new Vector3(0, 0, velocidad * Time.deltaTime));
+    }
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        if(collider.tag == "waypoint")
+        {
+            objetivo = collider.gameObject.GetComponent<Waypoints>().waypointProximo;
+            transform.LookAt(new Vector3(objetivo.position.x, transform.position.y, objetivo.position.z));
+        }
+    }
+}
