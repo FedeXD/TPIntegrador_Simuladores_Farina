4f47c1736bdf1e333e61f3936ecbb0917bcb1d96
Federico Farina <fedefarina02@gmail.com>
2021-12-12 02:19:06 -0300

    Empezar Carrera

---
+++ Assets/Scripts/Botones.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Botones : MonoBehaviour
+{
+    public void Jugar()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void Volver()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void Salir()
+    {
+        Application.Quit();
+    }
+
+    public void Reintentar()
+    {
+        SceneManager.LoadScene(2);
+    }
+}
+
+
+
